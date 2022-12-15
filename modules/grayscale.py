d1ca50746fe837a90fe8a3dab20b978319fc42ef
Nk777 <111414009+Nk777777@users.noreply.github.com>
2022-12-16 08:55:18 +0900

    Revert "add module"

--- modules/grayscale.py
+++
@@ -1,6 +0,0 @@
-import cv2
-
-def gray_scale(input_img):
-    img_bgr = cv2.imread('input_img/' + input_img)
-    img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
-    cv2.imwrite("images/gs/" + input_img ,img_gray)
