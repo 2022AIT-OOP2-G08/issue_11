2ca2cbb7c906b78ac0efde01d3a5204957429431
Sharu <104481020+SakaiHaruto@users.noreply.github.com>
2022-12-15 15:15:25 +0900

    webとくっつける

--- observation.py
+++ observation.py
@@ -1,21 +1,52 @@
-import sys
-import time
-import logging
+# ファイル変更イベント検出のため、watchdogをインポート
+from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
 
+# ファイルアクセスとスリープのため、osとtimeをインポート
+import os
+import time
+
+# 監視対象ディレクトリを指定する
+target_dir = 'input_img'
+
+# FileSystemEventHandler の継承クラスを作成
+class FileChangeHandler(FileSystemEventHandler):
+     # ファイル作成時のイベント
+     def on_created(self, event):
+         filepath = event.src_path
+         filename = os.path.basename(filepath)
+         print('%s created' % filename)
+
+
+    #  # ファイル変更時のイベント
+    #  def on_modified(self, event):
+    #      filepath = event.src_path
+    #      filename = os.path.basename(filepath)
+    #      print('%s changed' % filename)
+
+    #  # ファイル削除時のイベント
+    #  def on_deleted(self, event):
+    #      filepath = event.src_path
+    #      filename = os.path.basename(filepath)
+    #      print('%s deleted' % filename)
+
+    #  # ファイル移動時のイベント
+    #  def on_moved(self, event):
+    #      filepath = event.src_path
+    #      filename = os.path.basename(filepath)
+    #      print('%s moved' % filename)
+
+# コマンド実行の確認
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
-    observer.start()
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+     # ファイル監視の開始
+     event_handler = FileChangeHandler()
+     observer = Observer()
+     observer.schedule(event_handler, target_dir, recursive=True)
+     observer.start()
+     # 処理が終了しないようスリープを挟んで無限ループ
+     try:
+         while True:
+             time.sleep(0.1)
+     except KeyboardInterrupt:
+         observer.stop()
+     observer.join()
